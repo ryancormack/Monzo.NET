3d767fff8a2d1521ac874c584ca74b8f2791d28a
Ryan Cormack <ryancormack89@gmail.com>
2019-03-11 20:56:40 +0000

    Properly added RefreshRequest info

--- src/MonzoNet.Models/Authentication/RefreshRequest.cs
+++ src/MonzoNet.Models/Authentication/RefreshRequest.cs
@@ -10,21 +10,25 @@ namespace MonzoNet.Models.Authentication
         /// <summary>
         /// The type of Grant you are requesting. This is set to refresh_token
         /// </summary>
+        [JsonProperty("grant_type")]
         public string GrantType => "refresh_token";
 
         /// <summary>
         /// Your client ID.
         /// </summary>
+        [JsonProperty("client_id")]
         public string ClientId { get; set; }
 
         /// <summary>
         /// Your client secret.
         /// </summary>
+        [JsonProperty("client_secret")]
         public string ClientSecret { get; set; }
 
         /// <summary>
         /// The refresh token received along with the original access token.
         /// </summary>
+        [JsonProperty("refresh_token")]
         public string RefreshToken { get; set; }
     }
 }
